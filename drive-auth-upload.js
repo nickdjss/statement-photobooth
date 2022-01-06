93c300a0649f82edca0f534104554bf190ba482d
Nick DeJesus <ndejesus@marlton.com>
2022-01-06 16:26:13 -0500

    added prompts and google sheets

--- drive-auth-upload.js
+++ drive-auth-upload.js
@@ -2,14 +2,15 @@ const fs = require('fs');
 const readline = require('readline');
 const { google } = require('googleapis');
 
+
 // If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/drive.file'];
+const SCOPES = ['https://www.googleapis.com/auth/drive.file', 'https://www.googleapis.com/auth/spreadsheets'];
 const TOKEN_PATH = 'token.json';
 
 /**
  * Create an OAuth2 client with the given credentials, and then execute the given callback function.
  */
-function authorize(credentials, callback, filename) {
+function authorize(credentials, callback, filename, textResponses) {
 	return new Promise(function(resolve, reject){
   const {client_secret, client_id, redirect_uris} = credentials.installed;
   const oAuth2Client = new google.auth.OAuth2(
@@ -19,7 +20,7 @@ function authorize(credentials, callback, filename) {
   fs.readFile(TOKEN_PATH, (err, token) => {
     if (err) return getAccessToken(oAuth2Client, callback);
     oAuth2Client.setCredentials(JSON.parse(token));
-    callback(oAuth2Client, filename);
+    callback(oAuth2Client, filename, textResponses);
 	resolve("authorized");
   });
 	});
@@ -58,9 +59,11 @@ function getAccessToken(oAuth2Client, callback) {
 /**
 * Describe with given media and metaData and upload it using google.drive.create method()
 */ 
-function uploadFile(auth, filename) {
+function uploadFile(auth, filename, textResponses) {
+	updateSheet(auth, filename, textResponses);
 	return new Promise(function(resolve, reject){
   const drive = google.drive({version: 'v3', auth});
+  
   const fileMetadata = {
     'name': filename,
 	parents: ['1FufljJu_GMJqHEBYo5aUm3eLuK0p5O22']
@@ -85,4 +88,65 @@ function uploadFile(auth, filename) {
 	});
 }
 
-module.exports = {uploadFile, authorize}
+function updateSheet(auth, filename, textResponses) {
+  const sheets = google.sheets({version: 'v4', auth});
+  let values = [
+  textResponses,
+  // Additional rows ...
+];
+let resource = {
+  values,
+};
+
+   const request = {
+    spreadsheetId: '1JONIKWL88rdcTxl1IvXj0Wcm-YIHccbpemluk210moo', 
+    range: 'Sheet1!A2:E11',
+    // How the input data should be interpreted.
+    valueInputOption: '',  // TODO: Update placeholder value.
+
+    // How the input data should be inserted.
+    insertDataOption: '',  // TODO: Update placeholder value.
+
+    resource: {
+      values,
+    },
+
+    auth: auth,
+  };
+
+sheets.spreadsheets.values.append({
+  spreadsheetId:'1JONIKWL88rdcTxl1IvXj0Wcm-YIHccbpemluk210moo',
+  range:'Sheet1',
+ valueInputOption:'RAW',
+  resource:resource,
+}, (err, result) => {
+  if (err) {
+    // Handle error.
+    console.log(err);
+  } else {
+    console.log(`${result}`);
+  }
+});
+  
+  /*
+  sheets.spreadsheets.values.get({
+    spreadsheetId: '1JONIKWL88rdcTxl1IvXj0Wcm-YIHccbpemluk210moo',
+    range: 'Sheet1!A2:E11',
+  }, (err, res) => {
+    if (err) return console.log('The API returned an error: ' + err);
+    const rows = res.data.values;
+    if (rows.length) {
+      console.log('Name, Major:');
+      // Print columns A and E, which correspond to indices 0 and 4.
+      rows.map((row) => {
+        console.log(`${row[0]}, ${row[4]}`);
+      });
+    } else {
+      console.log('No data found.');
+    }
+  });
+  */
+}
+
+
+module.exports = {uploadFile, authorize, updateSheet}
--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const drive = require('./drive-auth-upload.js');
 const fs = require('fs');
 const uuidv4 = require('uuid');
 const PiCamera = require('pi-camera');
+const readline = require('readline');
+
+  
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 const myCamera = new PiCamera({
   mode: 'photo',
@@ -12,13 +19,13 @@ const myCamera = new PiCamera({
   nopreview: true,
 });
 
- const some_function = function(photoFileName){
+ const some_function = function(photoFileName, textResponses){
 	return new Promise(function(resolve, reject){
   
 		fs.readFile('credentials.json', (err, content) => {
 			if (err) {reject(Error(err))};
 			// Authorize a client with credentials, then call the Google Drive API.
-			drive.authorize(JSON.parse(content), drive.uploadFile, photoFileName).then(result =>{ 
+			drive.authorize(JSON.parse(content), drive.uploadFile, photoFileName, textResponses).then(result =>{ 
 			console.log(result);
 			resolve('success');
 			});
@@ -26,27 +33,51 @@ const myCamera = new PiCamera({
 		});
 	});
 };
- 
 
-
+  
+  
 if(environment == "prod"){
- myCamera.snap()
+		var recursiveAsyncReadLine = function () {
+	 rl.question('What is your name?', function (name) {
+    if (name == 'exit') //we need some base case, for recursion
+      return rl.close(); //closing RL and returning from function.
+	console.log("taking a photo");
+	 myCamera.snap()
   .then((result) => {
     console.log("image taken")
-	let uuid = uuidv4.v4(); 
-	let photoFileName = uuid += ".jpg";
-	some_function(photoFileName);
+		let uuid = uuidv4.v4(); 
+		let photoFileName = uuid += ".jpg";
+		let textResponses = [
+    'my name is:',name,"my file is: ",photoFileName
+  ];
+	some_function(photoFileName, textResponses).then(result => recursiveAsyncReadLine());
     // Your picture was captured
   })
   .catch((error) => {
     console.log(error);
      // Handle your error
   });
+  });
+
+}
+recursiveAsyncReadLine(); //Calling this function again to ask new question
 }
 
 if(environment == "dev"){
-	let uuid = uuidv4.v4(); 
-	let photoFileName = uuid += ".jpg";
-	some_function(photoFileName).then(result => console.log(result));
+	var recursiveAsyncReadLine = function () {
+	 rl.question('What is your name?', function (name) {
+    if (name == 'exit') //we need some base case, for recursion
+      return rl.close(); //closing RL and returning from function.
+	console.log("taking a photo");
+		let uuid = uuidv4.v4(); 
+		let photoFileName = uuid += ".jpg";
+		let textResponses = [
+    'my name is:',name,"my file is: ",photoFileName
+  ];
+		some_function(photoFileName, textResponses).then(result => recursiveAsyncReadLine());
+  });
+
 }
+recursiveAsyncReadLine(); //Calling this function again to ask new question
+};
   
