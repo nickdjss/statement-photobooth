6aca8c3078e57934a9e26ebbe5312d316f989db9
nick <nickdjss@gmail.com>
2022-01-12 10:31:52 -0500

    added screen and questions

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const fs = require('fs');
 const uuidv4 = require('uuid');
 const PiCamera = require('pi-camera');
 const readline = require('readline');
-const keyboard = 'keyboard-handler'
+const gkm = require('gkm');
+
+questions = ["What is your name?", "What is your favorite color?", "Where are you from?"];
+answers = ["","",""];
+
 
   
 const rl = readline.createInterface({
@@ -34,32 +38,15 @@ const upload = function(photoFileName, textResponses){
 	});
 };
 
-
-  
-  
-if(environment == "prod"){
-	let submit = false;
-	
-	while(!submit){
-keyboard.keyPressed(e => {
-  console.log(e.which)
-})
-	}
-	//display.updateText("what is your name");
-	let answer = "";
-	
-	
-	//display.updateText(name);
-	 
-	  
-	//console.log("taking a photo");
-	 /*myCamera.snap()
+function takePhoto(answers){
+  console.log("taking a photo");
+	 myCamera.snap()
   .then((result) => {
     console.log("image taken")
 		let uuid = uuidv4.v4(); 
 		let photoFileName = uuid += ".jpg";
 		let textResponses = [
-    'my name is:',name,"my file is: ",photoFileName
+    answers, "my file is: ",photoFileName
   ];
 	upload(photoFileName, textResponses).then(result => recursiveAsyncReadLine());
     // Your picture was captured
@@ -68,8 +55,54 @@ keyboard.keyPressed(e => {
     console.log(error);
      // Handle your error
   });
-  });
-  */
+  
+  }
+  
+  
+if(environment == "prod"){
+  display.updateText("");
+	let submit = "false";
+	let answer = "";
+	let allkeys = "1234567890-=QWERTYUIOPASDFGHJKLZXCVBNM<>?:{}!@#$%^&*()qwertyuiopasdfghjklzxcvbnm./,';][\`";
+	let question_index = 0;
+	console.log(questions[question_index]);
+gkm.events.on('key.*', function(data){
+
+  if (this.event != "key.released"){return;}
+  if (data == "Space"){answer += " "; return;}
+  if (data == "Slash"){answer += "/"; return;}
+  if (data == "Period"){answer += "."; return;}
+  if (data == "Comma"){answer += ","; return;}
+  if (data == "Quote"){answer += "'"; return;}
+  if (data == "Minus"){answer += "-"; console.log(answers); return;}
+  if (data == "Semicolon"){answer += ";"; return;}
+    console.log(data);
+  if (data == "Enter"){
+    answers[question_index] = answer;
+    question_index++;
+    if (question_index < questions.length){
+      console.log(questions[question_index]);
+      answer = "";
+     return;
+      }
+      takePhoto(answers)
+     answer = ""; 
+     question_index = 0;
+   answers = ["","",""];
+     return;}
+
+  if(allkeys.includes(data)){
+  answer += data;
+  display.updateText(answer);
+}
+});
+
+
+	//display.updateText("what is your name");
+	//display.updateText(name);
+	 
+	  
+	
 
 //}
 ///recursiveAsyncReadLine(); //Calling this function again to ask new question
